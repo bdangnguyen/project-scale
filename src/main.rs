14b77bb41f893dce6ae1c571cc2268acaa57985f
Bryan Nguyen <31323380+bdangnguyen@users.noreply.github.com>
2023-11-01 23:29:01 -0700

    Implement rendering player to screen

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,35 @@
-use rltk::{Rltk, GameState};
+use rltk::{GameState, Rltk, RGB};
+use specs::prelude::*;
 
+use specs_derive::Component;
 
-struct State;
+#[derive(Component)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Component)]
+struct Renderable {
+    glyph: rltk::FontCharType,
+    fg: RGB,
+    bg: RGB 
+}
+
+struct State {
+    ecs: World
+}
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();
-        ctx.print(1, 1, "Hello World");
+        
+        let positions = self.ecs.read_storage::<Position>();
+        let renderables = self.ecs.read_storage::<Renderable>();
+
+        for (pos, render) in (&positions, &renderables).join() {
+            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+        }
     }
 }
 
@@ -17,6 +40,22 @@ fn main() -> rltk::BError {
         .with_title("Project Scale")
         .build()?;
 
-    let gs = State;
+    let mut gs = State {
+        ecs: World::new()
+    };
+
+    gs.ecs.register::<Position>();
+    gs.ecs.register::<Renderable>();
+
+    gs.ecs
+        .create_entity()
+        .with(Position { x: 40, y: 25})
+        .with(Renderable {
+            glyph: rltk::to_cp437('@'),
+            fg: RGB::named(rltk::YELLOW),
+            bg: RGB::named(rltk::BLACK)
+        })
+        .build();
+
     rltk::main_loop(context, gs)
 }
